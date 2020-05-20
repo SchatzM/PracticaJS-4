a1765b141abe8ebffe9f65df39d74c7b3429e732
SchatzM <bri@npass.us>
2020-05-20 11:56:11 +0200

    Añadido nuevo método a primer objeto

--- js/funciones.js
+++ js/funciones.js
@@ -25,15 +25,19 @@ var miCasa = {
 		monedaValor: 'euros',
 		monedaValorSimbolo: '€'
 	},
-	mostrarInfo: function () { // Método para mostrar un resumen de las propiedades del objeto en consola.
+	mostrarResumen: function () { // Método para mostrar un resumen de las propiedades del objeto en consola.
 		let resumenDatosVivienda = 	`Vivienda ubicada en ${this.ubicacion.ciudad}, ${this.ubicacion.provincia}.\n`+
 									`Construida en ${this.detalles.anyoConstruccion} por constructora ${this.detalles.constructora}.\n`+
 									`se compone de ${this.detalles.tamanyo} con:\n`+
 									` · ${this.detalles.cantidadVentanas} ventanas\n · ${this.detalles.cantidadBanyo} baños\n · ${this.detalles.cantidadMuebles} muebles\n`+
-									`Precio del inmueble: ${this.precio.valor} ${this.precio.monedaValorSimbolo}`
-		console.info (resumenDatosVivienda)
+									`Precio del inmueble: ${this.precio.valor} ${this.precio.monedaValorSimbolo}` // Datos a mostrar en consola
+		console.info (resumenDatosVivienda); // Mostramos resumen en consola
+	},
+	mostrarPropiedadObjeto: function (propiedad) { // Método para iterar sobre los valores de una propiedad especificada del objeto y mostrarlos en consola
+		for (const [llave, valor] of Object.entries(this[propiedad])) {
+			console.info (`${llave}: ${valor}`); // Muestra en consola cada llave con su valor dentro de la propiedad especificada
+		}
 	}
-
 }
 
-miCasa.mostrarInfo (); // Muestra resumen de las propiedades del objeto miCasa
+miCasa.mostrarResumen (); // Muestra resumen de las propiedades del objeto miCasa
